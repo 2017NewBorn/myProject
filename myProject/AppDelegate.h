d821007c50faa42caa79c078b4247533a21ee7c8
digitSign <jiangfengxin_ios@sina.com>
2017-02-22 13:49:36 +0800

    myFisrtProject
    第一次上传

---
+++ myProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myProject
+//
+//  Created by 蒋锋鑫 on 2017/2/22.
+//  Copyright © 2017年 蒋锋鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myProject
+//
+//  Created by 蒋锋鑫 on 2017/2/22.
+//  Copyright © 2017年 蒋锋鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
